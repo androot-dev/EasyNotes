cd8a5ca5f70fdde4fafe6ef127c6f8305b7eda7a
Aledark <alejandrorgp18@gmail.com>
2020-04-16 12:12:38 -0400

    Mejoramiento en el posicionamiento de la note
    la nota se centra tomando en cuenta el scroll

--- contentScripts/contentScript.js
+++ contentScripts/contentScript.js
@@ -11,14 +11,17 @@ class note {
       let x = document.createElement('span');
       let capa = document.createElement('div');
       capa.id = "capa";
+
       note.id = 'note';
       textarea.id = 'paper';
       tack.id='tack';
       x.id = 'delete';
+      capa.style.height = document.body.clientHeight+'px';
       tack.appendChild(x);
       note.appendChild(tack);
       note.appendChild(textarea);
       capa.appendChild(note);
+      
 
       if(document.body.tagName == 'FRAMESET'){
         document.body.insertAdjacentHTML('afterend', capa.outerHTML);
@@ -26,17 +29,11 @@ class note {
         document.body.insertBefore(capa, document.body.children[0]);
       }
       let centerScroll = {left:note.offsetLeft, top:note.offsetTop}
-      capa.style.top = window.scrollY+'px';
-      capa.style.width = '100%';
-      capa.style.height = '100%';
       note.style.position = 'absolute';
-      note.style.top= centerScroll.top+"px";
-      note.style.left = centerScroll.left+"px";
-
+      note.style.top = (window.scrollY+(note.clientHeight/2))+"px";
+      note.style.left = centerScroll.left+"px"; 
     }
-    
   }
-  
 }
 class recivedMessageBackground extends note{
   constructor(callback){
@@ -44,18 +41,20 @@ class recivedMessageBackground extends note{
     chrome.runtime.onMessage.addListener ( (request, _, sendResponse) =>  {
      
       this.createNote(request);
-      this.setLink("https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap");
-
+      this.fontLoad("https://fonts.googleapis.com/css2?family=Comic+Neue&display=swap");
       callback();
 
       return true;
     });
   }
-  setLink(link){
-    let tag = document.createElement('link');
-    tag.href=link;
-    tag.rel ="stylesheet";
-    document.head.appendChild(tag);
+  fontLoad(link){
+    if(!document.getElementById('font-ready')){
+      let tag = document.createElement('link');
+      tag.href=link;
+      tag.rel ="stylesheet";
+      tag.id = "font-ready";
+      document.head.appendChild(tag);
+    }
   }
 }
 
@@ -67,105 +66,120 @@ class dragAndDrop{
     this.dropoutEvent = null;
 
 
-  var events=(el, key) => {
-    let elements = document.querySelectorAll(el.drag);
-    let drops = document.querySelectorAll(el.drop);
+  var events=(el, key, drop = false) => {
+    let elements = document.querySelectorAll(el);
 
+    let drops = document.querySelectorAll(this.arg[key].drop);
 
     for (let i = 0; i<elements.length; i++){
+      if(drop == false){
       elements[i].setAttribute('draggable', 'true');
-
+      
       elements[i].addEventListener('dragstart', (e) =>
-       this.dragstart(e, key), false);
+       this.dragstart(e, elements[i]), false);
+      } 
 
-      elements[i].addEventListener('drag', (e) => 
-        this.drag(e, key), false);
-
-      elements[i].addEventListener('dragend', (e) => 
-        this.dragend(e, key), false);
+      if(drop == true){
+        elements[i].addEventListener('dragover', (e) => 
+        this.dragover(e, elements[i]), false);
+     }
+      /* 
 
       elements[i].addEventListener('dragenter', (e) => 
-        this.dragenter(e, key), false);
+        this.dragenter(e, elements[i], drops), false);
 
-      elements[i].addEventListener('dragover', (e) => 
-        this.dragover(e, key), false);
+      elements[i].addEventListener('dragend', (e) => 
+        this.dragend(e, elements[i]), false);
 
       elements[i].addEventListener('dragleave', (e) => 
-        this.dragleave(e, key), false);
+        this.dragleave(e, elements[i]), false);
 
-      elements[i].addEventListener('drop', (e) => 
-        this.drop(e, key), false);
-    }
+       elements[i].addEventListener('drag', (e) => 
+        this.drag(e, elements[i]), false);
 
-    for (let i = 0; i<drops.length; i++){
+        elements[i].addEventListener('drop', (e) => 
+        this.drop(e, elements[i]), false);
+      */
       
-      drops[i].addEventListener('drop', (e) => 
-        this.drop(e, key), false);
-      drops[i].style.pointerEvents = 'all';
+     
     }
-    
   }
-  function getPosition(arg){ 
-      let id = arg.id.substring(1, arg.drag.length);
-   
-      var pos = {
-        top:document.getElementById(id).offsetTop - 10,
-        left:document.getElementById(id).offsetLeft - 10
-      }
-      return pos;
-  }
-
   for (let key = 0; key < arg.length; key++) {
-      events(arg[key], key);
+      events(arg[key].drag, key);
+      events(arg[key].drop, key, true);
   }
 }
-animateEvent(key, animation, event){
-    let animate = this.arg[key].animate && this.arg[key].animate.dragstart ? 
-    this.arg[key].animate.dragstart : null;
-
-    if(typeof(animate) == 'string' && 
-      this.animations[animate].to == 'dragstart') {
-      animation();
-    }
-}
-  dragstart(evt, key){
-    this.dragged=evt.target;
+  dragstart(evt, element){
+    console.log('dragstart in '+evt.target.id );
+    evt.target.style.opacity = '1';
+    this.dragged = evt.target;
   }
-  drag(evt, key){
+  drag(evt, element){
    evt.preventDefault();
+   console.log('drag in '+evt.target.id )
   }
-  dragenter(evt, key){
-    this.dropoutEvent = false;
-    this.dropped=evt.target;
+  dragenter(evt, element, drops){
+      evt.preventDefault();
+    var drop = false;
+    for (let key in drops){
+      if(drops[key] == element){
+        drop = true;
+        break;
+      }
+    }
+    if(drop == true){
+        console.log('DRAGENTER '+evt.target.id)
+        this.dropoutEvent = false;
+        this.dropped=evt.target;
+      }else {
+
+      
+      }
+  
   }
-  dragover(evt, key){
-    evt.preventDefault();
+  dragover(evt, element){
+   // console.log('dragover in '+evt.target.id )
+   //cuando se previene el comportamiento por defecto de este evento
+   //sequitan las limitaciones de drop, convirtiendo las zonas en droppables.
+    if(evt.target.id == 'capa' /*droppables*/ ){
+       evt.preventDefault();
+    }
+ 
+
   }
-  dragleave(evt, key){
+  dragleave(evt, element){
+    console.log('dragleave in '+evt.target.id )
     evt.preventDefault();
     this.dropoutEvent = true;
     this.dropped=null;
   }
-  dragend(evt,  key){
-     
+  dragend(evt,  element){
+    console.log('dragend in '+evt.target.id )
     if(this.dropoutEvent == true){this.dropout();}
   }
-  drop(evt, key){
-    // no quiere accionarse este evento
-    console.log('ESTA MIERDA NO APARECE')
-    if (this.dragged!=null) {
+  drop(evt, element){
 
-      let x = window.event.clientX;
-      let y = window.event.clientY;
-
-      this.dragged.style.top = y+'px';
-      this.dragged.style.left = x+'px';
+    console.log('drop in '+evt.target.id )
+    function mousePosition(elemento, evt) {
+        var ClientRect = elemento.getBoundingClientRect();
+        return { //objeto
+        x: Math.round(evt.clientX - ClientRect.left),
+        y: Math.round(evt.clientY - ClientRect.top)
+         }
+      }
+    if (this.dragged!=null) { 
+    let pos = mousePosition(evt.target, evt);
+    let centro = {
+        width: this.dragged.clientWidth /2,
+        height: this.dragged.clientHeight /2
+      } 
+      this.dragged.style.top = (pos.y-centro.height)+'px';
+      this.dragged.style.left = (pos.x-centro.width)+'px';
       this.dropped=null;
       this.dragged=null;
     }
   }  
    dropout(){ /* my event */
-    console.log('dd')
     if(this.dragged != null){
      
     }
@@ -178,7 +192,7 @@ animateEvent(key, animation, event){
 
      new dragAndDrop([{
       drag:"#note", //draggable element
-      drop: "body"
+      drop: "#capa"
     }]);
 
  });
