dab435afc843c2189867b609602e886cdf1d05db
Aledark <alejandrorgp18@gmail.com>
2020-04-18 19:08:56 -0400

    Class colors creada
    ahora se almacena el color prferido en el popup usando chrome.storage.sync

--- index.js
+++ index.js
@@ -2,23 +2,45 @@ class colors{
 	constructor(note, font){
 		this.note = note;
 		this.font = font;
-		this.setNotePreferedColor();
+		this.setListPreferedColor();
+		this.getSelectedColor();
 
 	}
-	setNotePreferedColor(){
-		function elPrefered(val){
+	setColorNote(keySelect, auto = false){
+		let prefered = document.querySelectorAll('.prefered')
+
+		prefered.forEach( function(element, index) {
+			element.style.transform = 'scale(1)';
+		});
+
+		document.getElementById('colorNote-'+keySelect).style.transform = 'scale(1.4)';
+		document.getElementById('miniNote').style.background = this.note[keySelect];
+		if(auto!=true){
+			chrome.storage.sync.set({key: keySelect}, function() {
+          		console.log('se guardo el indice'+keySelect );
+        	});
+		}
+	}
+	getSelectedColor(){
+		chrome.storage.sync.get(['key'], (result) => {
+			console.log(result)
+			if(result.key){
+				this.setColorNote(result.key, true);
+			}
+        });
+	}
+	setListPreferedColor(){
+		function prefered(val){
 			return document.getElementById('colorNote-'+val);
 		}
 
 		for(let i=0; i <= 5; i++){
-			elPrefered(i).style.background = this.note[i];
-			elPrefered(i).addEventListener('click',()=>{
-				document.getElementById('miniNote').style.background = this.note[i];
+			prefered(i).style.background = this.note[i];
+			prefered(i).addEventListener('click',()=>{
+				this.setColorNote(i);
 			}, false);
 		}
 	}
-	
-
 }
 
 new colors(['#2f3640', '#fd9644', '#f1c40f', '#26de81', '#2bcbba', '#9c88ff'],'');
