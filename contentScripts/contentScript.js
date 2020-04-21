de56a7f01b7e2c5f65995f1439a44076f021e721
Aledark <alejandrorgp18@gmail.com>
2020-04-21 15:17:12 -0400

    Clase dragDrop mejorada
    los eventos drag y dragover ahora posicionan bien la nota
    falta aplicar el evento drop

--- contentScripts/contentScript.js
+++ contentScripts/contentScript.js
@@ -24,8 +24,14 @@ class note extends font {
   createNote(request){
       const create = function(tag, attr){ 
         let el = document.createElement(tag);
-        el.id = attr.id;
-        el.className+=attr.class;
+        for (let key in attr){
+          if(key == 'class'){
+            el.classList+= attr[key];
+          }else{
+            el[key] = attr[key];
+          }
+          
+        }
         return el;
       }
       const noteModel = {
@@ -33,7 +39,11 @@ class note extends font {
         area: create ('div',{class:"areaEx0A", id:"areaEx"+(this.countNote+1)}),
         tack: create ('div',{class:"tackEx0A", id:"tackEx"+(this.countNote+1)}),
         span: create ('span',{class:"removeEx0A", id:"removeEx"+(this.countNote+1)}),
-        text: create ('textarea',{class:"paperEx0A", id:"paperEx"+(this.countNote+1)}),
+        text: create ('div',{
+          class:"paperEx0A", 
+          id:"paperEx"+(this.countNote+1),
+          contentEditable: "true"
+        }),
         generate: function(){
           this.tack.appendChild(this.span);
           this.note.appendChild(this.tack);
@@ -93,14 +103,23 @@ class comunicationContentScript extends note{
     });
   }
 }
-
 class DragDrop{
   constructor(drag, drop){
     const $ = (sel) => { 
       let el = document.querySelectorAll(sel); //selecciona lista de elementos
       el.on = (evt) =>{ // añade un evento a todos los elementos de una lista 
         el.forEach( (element, index) => {
+
           if(evt == 'dragstart'){element.setAttribute('draggable', 'true')}
+            element.mouseX = function(evt){ //obtiene la posicion x del mouse
+              let ClientRect = this.getBoundingClientRect();
+              return Math.round(evt.clientX - ClientRect.left)
+            },
+            element.mouseY = function(evt){
+              let ClientRect = this.getBoundingClientRect();
+              return Math.round(evt.clientY - ClientRect.top)
+            }
+
             element.addEventListener(evt, (e) => 
             this[evt](e), false);
         });
@@ -108,85 +127,88 @@ class DragDrop{
       return el; }
     this.dragElement = drag;
     this.dropElement = drop;
-    this.IDelement;
     this.dragged = null;
     this.dropped = null;
     this.dropoutEvent = null;
-   
+    this.dataEvents = {};
     $(this.dragElement).on('dragstart');
     $(this.dropElement).on('dragover');
     $(this.dropElement).on('drop');
     $(this.dragElement).on('drop');
   }
-  
+  setData(name, val){
+     this.dataEvents[name] = val;
+  }
+  getData(nameProp){
+    return this.dataEvents[nameProp];
+  }
   dragstart(evt){
-    function mousePosition(id) {
-        var ClientRect = document.getElementById(id).getBoundingClientRect();
-        return { //objeto
-          x: Math.round(evt.clientX - ClientRect.left),
-          y: Math.round(evt.clientY - ClientRect.top)
-        }
-    }
-    let intID = evt.target.id.replace('noteEx', '');
-    let area = document.getElementById("areaEx"+intID);
-    area.style.visibility = 'visible';
-    evt.target.style.opacity = '1';
-    this.dragged = evt.target;
-    this.IDelement = intID;
-    let pos = mousePosition(evt.target.id);
-    let posArea = mousePosition("areaEx"+this.IDelement);
-    evt.dataTransfer.setData('x', pos.x);
-    evt.dataTransfer.setData('y', pos.y);
-    let img = document.createElement('img');
 
-    evt.dataTransfer.setDragImage(img, 0, 0);
-    evt.dataTransfer.effectAllowed = "none";
+    if(evt.target.mouseX){
+      let drag ={
+        getArea: function(){
+        let id ;
+
+        for(let i =3; i<evt.target.id.length; i++){
+          if(evt.target.id[i] == 'E'){
+            id = evt.target.id.substring(i+2, evt.target.id.length);
+            break;
+          }
+        }
+        return document.getElementById("areaEx"+id);
+       },
+      css_start:function(){
+        this.getArea().style.visibility = 'visible';
+        evt.target.style.opacity = '1';
+      },
+      props_global:()=>{
+         this.dragged = evt.target;
+         this.setData('idArea', evt.target.id.replace('noteEx', ''));
+      },
+      preventGhost:function(){
+        let img = document.createElement('img');
+        evt.dataTransfer.setDragImage(img, 0, 0);
+      }
+     }
+      drag.css_start();
+      drag.props_global();
+      this.setData('x', evt.target.mouseX(evt))
+      this.setData('y', evt.target.mouseY(evt))
+      drag.preventGhost();
+    }else{
+      evt.preventDefault()
+    }
   }
   dragover(evt){
-    function mousePosition(id) {
-      var ClientRect = document.getElementById(id).getBoundingClientRect();
-      return { //objeto
-        x: Math.round(evt.clientX - ClientRect.left),
-        y: Math.round(evt.clientY - ClientRect.top)
-      }
-    }
-    let posMouse = mousePosition("areaEx"+this.IDelement);
-     let positionPressMouseNote = {
-        x: evt.dataTransfer.getData('x'),
-        y: evt.dataTransfer.getData('y')
-      } 
-      this.dragged.style.top = (posMouse.y-positionPressMouseNote.y)+'px';
-      this.dragged.style.left= (posMouse.x-positionPressMouseNote.x)+'px';
-    if(evt.target.classList[0] == 'areaEx0A' ||  
-       evt.target.classList[0] == 'noteEx0A' ||
-       evt.target.classList[0] == 'paperEx0A' ||
-       evt.target.classList[0] == 'tackEx0A'){
-      evt.preventDefault();
-    }
-  }
-  drop(evt){
-    function mousePosition(id) {
-      var ClientRect = document.getElementById(id).getBoundingClientRect();
-      return { //objeto
-        x: Math.round(evt.clientX - ClientRect.left),
-        y: Math.round(evt.clientY - ClientRect.top)
-      }
-    }
-    console.log(evt.target.classList[0])
-    if (this.dragged!=null && evt.target.classList[0] == 'areaEx0A') { 
-      let posMouse = mousePosition("areaEx"+this.IDelement);
-      let positionPressMouseNote = {
-          x: evt.dataTransfer.getData('x'),
-          y: evt.dataTransfer.getData('y')
-        } 
-        evt.target.style.visibility = 'hidden';
-        this.dragged.style.top = (posMouse.y-positionPressMouseNote.y)+'px';
-        this.dragged.style.left= (posMouse.x-positionPressMouseNote.x)+'px';
-        this.dropped=null;
-        this.dragged=null;
 
+    let over = {
+
+      area: document.getElementById('areaEx'+this.getData('idArea')),
+      move: (area)=>{
+        this.dragged.style.top =area.mouseY(evt)-this.getData('y') + 'px';
+        this.dragged.style.left =area.mouseX(evt)-this.getData('x') + 'px';
+      },
+      setDropZones(arrayClass){
+        //solo funciona si el dropzone tiene una sola clase
+        let drop = false;
+        for (let i in arrayClass){
+          if(arrayClass[i] == evt.target.classList[0]){
+            drop == true;
+            break;
+          }
+        }
+        if(drop == true){
+           evt.preventDefault();
+        }
       }
-  } 
+    }
+    over.move(over.area);
+    over.setDropZones(['areaEx0A', 'noteEx0A', 'paperEx0A', 'tackEx0A']);
+  }
+  drop(){
+    
+  }
+  
   /* end class */
 }
  new comunicationContentScript(() =>{
